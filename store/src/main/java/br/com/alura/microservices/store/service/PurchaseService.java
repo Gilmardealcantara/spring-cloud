1dd4f79a19f434382d0584ed28d1134ba7708fef
Gilmar <gilmardealcantara@gmail.com>
2021-01-03 12:46:56 -0300

    create eureka service and registration supplier and store

--- store/src/main/java/br/com/alura/microservices/store/service/PurchaseService.java
+++ store/src/main/java/br/com/alura/microservices/store/service/PurchaseService.java
@@ -2,6 +2,7 @@ package br.com.alura.microservices.store.service;
 
 import br.com.alura.microservices.store.controller.dto.InfoSupplier;
 import br.com.alura.microservices.store.controller.dto.PurchaseDto;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpMethod;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Service;
@@ -9,9 +10,11 @@ import org.springframework.web.client.RestTemplate;
 
 @Service
 public class PurchaseService {
+    @Autowired
+    private RestTemplate client;
+
     public void makePurchase(PurchaseDto purchase){
-        RestTemplate client = new RestTemplate();
-        ResponseEntity<InfoSupplier> exchange = client.exchange("http://localhost:8081/info/" + purchase.getAddress().getState(), HttpMethod.GET, null, InfoSupplier.class);
+        ResponseEntity<InfoSupplier> exchange = client.exchange("http://supplier/info/" + purchase.getAddress().getState(), HttpMethod.GET, null, InfoSupplier.class);
         System.out.printf(exchange.getBody().getAddress());
     }
 }
